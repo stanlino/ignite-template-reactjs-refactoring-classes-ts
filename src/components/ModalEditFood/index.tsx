5368a87da597e08c24a7d15f5e4c73320a5a46cc
Stanley Filipe <stanley92filipe@gmail.com>
2022-06-02 15:28:07 -0300

    refactor: add typescript and remove class

---
+++ src/components/ModalEditFood/index.tsx
@@ -0,0 +1,55 @@
+import { createRef, useRef } from 'react';
+import { FiCheckSquare } from 'react-icons/fi';
+
+import { Form } from './styles';
+import Modal from '../Modal';
+import Input from '../Input';
+
+type Food = {
+  name: string
+  description: string
+  price: number
+  image: string
+}
+
+interface ModallEditFoodProps {
+  isOpen: boolean
+  setIsOpen: () => void
+  editingFood: Food
+  handleUpdateFood(data: Food): void
+}
+
+function ModalEditFood(props : ModallEditFoodProps) {
+  const formRef = useRef(null)
+
+  const { isOpen, setIsOpen, editingFood, handleUpdateFood } = props;
+
+  async function handleSubmit(data: Food) {
+
+    handleUpdateFood(data);
+    setIsOpen();
+  };
+
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <Form ref={formRef} onSubmit={handleSubmit} initialData={editingFood}>
+        <h1>Editar Prato</h1>
+        <Input name="image" placeholder="Cole o link aqui" />
+
+        <Input name="name" placeholder="Ex: Moda Italiana" />
+        <Input name="price" placeholder="Ex: 19.90" />
+
+        <Input name="description" placeholder="Descrição" />
+
+        <button type="submit" data-testid="edit-food-button">
+          <div className="text">Editar Prato</div>
+          <div className="icon">
+            <FiCheckSquare size={24} />
+          </div>
+        </button>
+      </Form>
+    </Modal>
+  );
+};
+
+export default ModalEditFood;
